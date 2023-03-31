1fc5f5d7e8fd37cc9bef993762adbd976c0e7da5
Arshaluys Arakelyan <arshaluys.arakelyan@globallogic.com>
2023-03-31 15:45:45 +0200

    test checks

--- container/src/App.js
+++ container/src/App.js
@@ -4,6 +4,7 @@ import { MarketingAppComponent } from "./components/MarketingAppComponent";
 export const App = () =>{
     return <div>
          <h1>Container!!!!</h1>
+         <h2>Here we are</h2>
          <hr/>
          <MarketingAppComponent />
     </div>
