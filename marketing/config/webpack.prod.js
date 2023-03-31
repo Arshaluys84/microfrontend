8a02e42b364f04a13dbeb19df6618755268ceec6
Arshaluys Arakelyan <arshaluys.arakelyan@globallogic.com>
2023-03-31 14:43:09 +0200

    add publicPath

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -7,7 +7,7 @@ const packageJson = require('../package.json')
 const prodConfig = {
     mode: "production",
     output:{
-        filename: "[name].[contenthash].js"
+        filename: "[name].[contenthash].js",
     },
     plugins:[
         new ModuleFederationPlugin({
