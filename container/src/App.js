8a52b3583cad08105ae1800443ae5e5a92d9f5b2
Arshaluys Arakelyan <arshaluys.arakelyan@globallogic.com>
2023-03-31 15:28:51 +0200

    some updates

--- container/src/App.js
+++ container/src/App.js
@@ -3,7 +3,7 @@ import { MarketingAppComponent } from "./components/MarketingAppComponent";
 
 export const App = () =>{
     return <div>
-         <h1>Container</h1>
+         <h1>Container!!!!</h1>
          <hr/>
          <MarketingAppComponent />
     </div>
