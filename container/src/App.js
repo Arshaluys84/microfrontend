5d147078d83fb1d01f6e552db75852ae48e44346
Arshaluys Arakelyan <arshaluys.arakelyan@globallogic.com>
2023-04-03 14:55:52 +0200

    add dashboard and deploy

--- container/src/App.js
+++ container/src/App.js
@@ -1,24 +1,35 @@
-import React, { lazy, Suspense, useState } from "react";
+import React, { lazy, Suspense, useState, useEffect } from "react";
 
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Router, Switch, Route, Redirect } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
+import { createBrowserHistory } from "history";
 
 import Header from "./components/Header";
 import Progress from "./components/Progress";
 
 const AuthLazy = lazy(() => import("./components/AuthAppComponent"));
 const MarketingLazy = lazy(() => import("./components/MarketingAppComponent"));
+const DashboardLazy = lazy(() => import("./components/DashboardAppComponent"));
+
+const history = createBrowserHistory();
 
 export const App = () => {
   const [isSignin, setIsSignin] = useState(false);
   const generateClassName = createGenerateClassName({
     productionPrefix: "co",
   });
+
+  useEffect(() => {
+    if (isSignin) {
+      history.push("/dashboard");
+    }
+  }, [isSignin]);
+
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header isSignin={isSignin} onSignOut={() => setIsSignin(false)} />
@@ -27,11 +38,15 @@ export const App = () => {
               <Route path="/auth">
                 <AuthLazy onSignin={() => setIsSignin(true)} />
               </Route>
+              <Route path="/dashboard">
+                {!isSignin && <Redirect to="/" />}
+                <DashboardLazy />
+              </Route>
               <Route path="/" component={MarketingLazy} />
             </Switch>
           </Suspense>
         </div>
       </StylesProvider>
-    </BrowserRouter>
+    </Router>
   );
 };
--- dashboard/config/webpack.dev.js
+++ dashboard/config/webpack.dev.js
@@ -7,19 +7,23 @@ const commonConfig = require("./webpack.common");
 const devConfig = {
   mode: "development",
   output: {
-    publicPath: "http://localhost:8080/",
+    publicPath: "http://localhost:8083/",
   },
   devServer: {
-    port: 8080,
-    historyApiFallback: true,
+    port: 8083,
+    historyApiFallback: {
+      index: "/index.html",
+    },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
   },
   plugins: [
     new ModulePlugin({
-      name: "container",
-      remotes: {
-        marketing: "marketing@http://localhost:8081/remoteEntry.js",
-        auth: "auth@http://localhost:8082/remoteEntry.js",
-        dashboard: "dashboard@http://localhost:8083/remoteEntry.js",
+      name: "dashboard",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./DashboardApp": "./src/bootstap",
       },
       shared: packageJson.dependencies,
     }),
